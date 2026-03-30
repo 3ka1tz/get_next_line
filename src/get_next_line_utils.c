95fe584c4c168b5ac849c7edbdd27fc47489aa9d
Ekaitz López <ekaitzlopez2025@gmail.com>
2026-03-30 22:58:44 +0200

    Update

---
+++ src/get_next_line_utils.c
@@ -0,0 +1,57 @@
+#include "get_next_line.h"
+
+size_t	gnl_strlen(char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*gnl_strchr(char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*gnl_strjoin(char *s1, char *s2)
+{
+	char	*result;
+	size_t	i;
+	size_t	j;
+
+	if (!s1)
+	{
+		s1 = malloc(1);
+		if (!s1)
+			return (NULL);
+		s1[0] = '\0';
+	}
+	result = malloc(gnl_strlen(s1) + gnl_strlen(s2) + 1);
+	if (!result)
+		return (free(s1), NULL);
+	i = 0;
+	while (s1[i])
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		result[i++] = s2[j++];
+	result[i] = '\0';
+	return (free(s1), result);
+}
